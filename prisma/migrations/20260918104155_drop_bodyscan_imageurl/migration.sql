438c21ec83c110f0f7b484b437e3f08b58042797
George Milojevic <ai.george.assistant@gmail.com>
2026-09-18 13:42:17 +0300

    migration: drop BodyScan.imageUrl

---
+++ prisma/migrations/20260918104155_drop_bodyscan_imageurl/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageUrl` on the `BodyScan` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BodyScan" DROP COLUMN "imageUrl";
